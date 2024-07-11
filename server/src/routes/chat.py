6d1cb89d4efb81b181e8f26f76ead7eb084bfcd8
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-07-11 12:20:00 +0100

    feat: run Producer to add data to the stream using redis_client

--- server/src/routes/chat.py
+++ server/src/routes/chat.py
@@ -7,11 +7,16 @@ import uuid
 
 from ..socket.connection import ConnectionManager
 from ..socket.utils import get_token
+from ..redis.producer import Producer
+from ..redis.config import Redis
 
 chat = APIRouter()
 
 # initialize the connection manager
 manager = ConnectionManager()
+
+# intialize redis connection
+redis = Redis()
 
 # @route    POST /token
 # @desc     Route to generate chat token
@@ -31,7 +36,7 @@ async def token_generator(name: str, request: Request):
         raise HTTPException(status_code=400, detail={
             "loc": "name", "msg": "Enter a valid name"
         })
-    
+
     # Generate a token using uuid4
     token = str(uuid.uuid4())
 
@@ -63,6 +68,13 @@ async def websocket_endpint(
 ):
     # add the new websocket to the connction manager
     await manager.connect(websocket)
+
+    # create a new Redis connection
+    redis_client = await redis.create_connection()
+
+    # initialize a producer with redis_client
+    producer = Producer(redis_client)
+
     try:
         # run a while true loop to ensure the socket stays open
         while True:
@@ -72,9 +84,16 @@ async def websocket_endpint(
             # TODO: send the received message from the client the AI model,
             # and send, the response from the AI model, back to the client
 
+            # initialize stream data dictionnary
+            stream_data = {}
+            # add `data` to `stream_data` with the key `token`
+            stream_data[token] = data
+            # add `stream_data` to the stream using redis client `producer`
+            await producer.add_to_stream(stream_data, "message_channel")
             # send a hard coded response back to the client - for now
             await manager.send_personal_message(
-                "Response: Simulating response from the GPT service.", websocket
+                "Response: Simulating response from the GPT service.",
+                websocket
             )
     # execept when the socket gets disconnected
     except WebSocketDisconnect:
