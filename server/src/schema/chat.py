af198ac48d4aac2a18e64532c1d0dcd8fdcb0a5b
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-07-15 16:37:06 +0100

    fix: update attributes so that all of them be annotated attributes thus avoiding PydanticUserError exception

--- server/src/schema/chat.py
+++ server/src/schema/chat.py
@@ -1,19 +1,19 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Optional
 import uuid
 
-
 class Message(BaseModel):
     """Message class"""
-    id = uuid.uuid4()  # chat id
+    id: uuid.UUID = Field(default_factory=uuid.uuid4)  # chat id
     msg: str  # message text
-    timestamp = str(datetime.now())  # automatically generated timestamp
-
+    # automatically generated timestamp
+    timestamp: str = Field(default_factory=lambda: str(datetime.now()))
 
 class Chat(BaseModel):
     """A class that will hold data about a single Chat session"""
     token: str  # token
     messages: List[Message]
     name: str  # name of the user
-    session_start = str(datetime.now())  # automatically generated timestamp
+    # automatically generated timestamp
+    session_start: str = Field(default_factory=lambda: str(datetime.now()))
