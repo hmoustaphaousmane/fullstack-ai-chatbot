c502de1d1cfcbfeaeb9c3d8ba53df1387ac5007f
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-08-01 11:24:24 +0100

    feat: create Message model and refactor cache and main script

--- worker/main.py
+++ worker/main.py
@@ -1,7 +1,13 @@
+import os
 import asyncio
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
 
 from src.redis.config import MyRedis
 from src.redis.cache import Cache
+from src.model.gptj import GPT
+from src.schema.chat import Message
 
 
 async def main():
@@ -12,7 +18,8 @@ async def main():
 
     # add a new message to the chat history
     await Cache(json_client).add_message_to_cache(
-        token="1ea18262-d763-4bc8-86f2-e9c9fc419e98",
+        token=os.getenv('token'),
+        source="human",
         message_data={
             "id": "1",
             "msg": "Hello",
@@ -21,9 +28,31 @@ async def main():
     )
 
     data = await Cache(json_client).get_chat_history(
-        token="1ea18262-d763-4bc8-86f2-e9c9fc419e98"
+        token=os.getenv('token')
     )
-    print(data)
+    # print(data)
+
+    # trim the cache data to get the last 4 messages
+    message_data = data['messages']#[-4]
+    print(message_data)
+
+    # extract the msg in a list
+    input = ["" + i['msg'] for i in message_data]
+    input = " ".join(input)  # join it to an empty string
+
+    res = GPT().query(input=input)
+
+    # create a new Message for the bot response
+    msg = Message(msg=res)
+    print(msg)
+
+    # add the bot response to the cache specifying the source as "bot"
+    await Cache(json_client).add_message_to_cache(
+        token=os.getenv('token'),
+        source="bot",
+        message_data=dict(msg)  #.dict()
+    )
+
     # # create a Redis connection pool
     # redis = await redis.create_connection()
     # print(redis)
--- worker/src/redis/cache.py
+++ worker/src/redis/cache.py
@@ -15,16 +15,21 @@ class Cache:
         chat_session = json.loads(data)
         return chat_session
 
-    async def add_message_to_cache(self, token: str, message_data: dict):
+    async def add_message_to_cache(self, token: str, source: str, message_data: dict):
         # fetch the existing chat session
         data = self.json_client.get(str(token))
         if data is None:
             raise ValueError("Chat session not found for the given token.")
         
         chat_session = json.loads(data)
-        print(chat_session)
+        # print(chat_session)
 
-        # sdd the new message to the list of messages
+        if source == "human":
+            message_data['msg'] = "Human: " + (message_data['msg'])
+        elif source == "bot":
+            message_data['msg'] = "Bot: " + (message_data['msg'])
+
+        # add the new message to the list of messages
         chat_session['messages'].append(message_data)
 
         # store the updated chat session back into the cache
